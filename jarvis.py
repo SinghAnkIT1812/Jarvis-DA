ace1fadcde1858e727091ae2369509f23526a31b
Ankit Kumar Singh <77728943+SinghAnkIT1812@users.noreply.github.com>
2021-02-04 20:42:44 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -6,6 +6,7 @@ import webbrowser
 import os
 import smtplib
 
+
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 # print(voices[1].id)
@@ -13,11 +14,13 @@ engine.setProperty('voice', voices[0].id)
 
 
 def speak(audio):
+    
     engine.say(audio)
     engine.runAndWait()
 
 
 def wishMe():
+    
     hour = int(datetime.datetime.now().hour)
     if hour>=0 and hour<12:
         speak("Good Morning!")
